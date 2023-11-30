3d52d9b1a911b76ba4685e37b6f2971f53ae712d
george duan <george.duan@linecorp.com>
2023-11-30 18:57:16 +0800

    Add line hook

--- src/app/api/lineHook/route.ts
+++ src/app/api/lineHook/route.ts
@@ -1,20 +1,38 @@
 
 import * as line from '@line/bot-sdk';
+import {createEdgeRouter, expressWrapper} from 'next-connect';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { NextResponse } from 'next/server';
+import type {NextRequest} from 'next/server';
+
 const config = {
   channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN || '',
   channelSecret: process.env.LINE_CHANNEL_SECRET || '',
 }
 
-const lineMiddleware = line.middleware(config);
-
-const handler = (req: any, res: any) => {
-  if (req.method === 'POST') {
-    lineMiddleware(req, res, () => {
-      res.json({ message: 'OK' });
-    });
-  } else {
-    res.status(405).json({ message: 'Method Not Allowed' });
-  }
+interface RequestContext {
+  params: {
+    id: string;
+  };
 }
 
-export { handler as GET, handler as POST };
+const lineMiddleware = line.middleware(config);
+
+const router = createEdgeRouter<NextRequest, RequestContext>();
+router
+  // @ts-ignore
+  .use(expressWrapper(lineMiddleware))
+  .get((req) => {
+    return NextResponse.json({ hello: 'world' })
+  })
+  .post((req) => {
+    return NextResponse.json({ hello: 'world' })
+  })
+
+export async function GET(request: NextRequest, ctx: RequestContext) {
+  router.run(request, ctx);
+}
+
+export async function POST(request: NextRequest, ctx: RequestContext) {
+  router.run(request, ctx);
+}
